e98b58be358615335d9767114ef8f443216bb8ed
kim hogan <kimhogan7495@gmail.com>
2021-02-07 07:57:43 -0500

    2nd commit

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b100eed32b9796c38620ceec21d97ae9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBpPlqkiRkn6Y8l22TT_xyAPAto3rSHLBk"
